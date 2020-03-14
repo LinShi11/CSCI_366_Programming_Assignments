0fdd59ba558f0757fe09d44b3186d50ec8247a5a
LinShi11 <50639335+LinShi11@users.noreply.github.com>
2020-03-13 19:19:55 -0600

    Add files via upload

--- src/Client.cpp
+++ src/Client.cpp
@@ -18,29 +18,182 @@
 #include "Client.hpp"
 
 Client::~Client() {
+    remove(board_name.c_str());
 }
 
 
 void Client::initialize(unsigned int player, unsigned int board_size){
+
+    std::string out_string;
+    std::stringstream ss;
+    ss << player;
+    out_string = ss.str();
+    ofstream board_name("player_" + out_string + ".action_board.json");
+    string coord = "{\n    \"board\": [\n";
+
+    for(int i = 0; i < board_size; i++){
+        coord += "        [\n";
+        for(int j = 0; j < board_size; j++){
+            if(j != board_size-1){
+                coord += "            0,\n";
+            }
+            else{
+                coord += "            0\n";
+            }
+        }
+        if(i != board_size-1) {
+            coord += "        ],\n";
+        }
+        else {
+            coord += "        ]\n";
+        }
+    }
+    coord += "    ]\n";
+    coord += "}";
+    board_name << coord;
+    board_name.close();
+    this->board_size = board_size;
+    if(player == 2){
+        initialized = true;
+    }
+    this->player = 1;
 }
 
 
 void Client::fire(unsigned int x, unsigned int y) {
+    ofstream output;
+
+    int temp = this->player%2;
+    if(temp==1){
+        output.open("player_1.shot.json");
+    }
+    else{
+        output.open("player_2.shot.json");
+    }
+
+    string coords = "{\n    \"x\": "+to_string(x)+",\n    \"y\": "+to_string(y)+"\n}";
+    output << coords;
+
 }
 
 
 bool Client::result_available() {
+    ifstream input;
+    int temp = this->player%2;
+    if(temp==1){
+        input.open("player_1.result.json");
+    }
+    else{
+        input.open("player_2.result.json");
+    }
+
+    string result_str = "{\n"
+                        "    \"result\": "+to_string(1)+"\n"
+                                                             "}";
+    if(input.is_open()){
+        return true;
+    }
+
 }
 
 
 int Client::get_result() {
+    ifstream input;
+    int temp = this->player%2;
+    if(temp==1){
+        input.open("player_1.result.json");
+    }
+    else{
+        input.open("player_2.result.json");
+    }
+    noskipws(input);
+    istreambuf_iterator<char> head(input);
+    istreambuf_iterator<char> tail;
+    string data(head, tail);
+    char result;
+
+    for(int i =0; i < sizeof(data); i++){
+        if(data[i] == 'r'){
+
+            result = data[i+9] ;
+            break;
+        }
+    }
+    input.close();
+    remove("player_1.result.json");
+    remove("player_2.result.json");
+    if(result == '1'){
+        return HIT;
+    }
+    else if (result == '-'){
+        return MISS;
+    }
+    else if (result == '0'){
+        return OUT_OF_BOUNDS;
+    }
+    else{
+        throw(ClientException("Bad result"));
+    }
 }
 
 
 
 void Client::update_action_board(int result, unsigned int x, unsigned int y) {
+    ofstream board_name;
+    int temp = this->player%2;
+    if(temp==1){
+        board_name.open("player_1.action_board.json");
+    }
+    else{
+        board_name.open("player_2.action_board.json");
+    }
+
+    string coord = "{\n    \"board\": [\n";
+
+    for(int i = 0; i < board_size; i++){
+        coord += "        [\n";
+        for(int j = 0; j < board_size; j++){
+            if(i == y and j == x){
+                coord += "            "+ to_string(result) + ",\n";
+            }
+            else {
+                if (j != board_size - 1) {
+                    coord += "            0,\n";
+                } else {
+                    coord += "            0\n";
+                }
+            }
+        }
+        if(i != board_size-1) {
+            coord += "        ],\n";
+        }
+        else {
+            coord += "        ]\n";
+        }
+    }
+
+    coord += "    ]\n";
+    coord += "}";
+    board_name << coord;
+    board_name.close();
+
 }
 
 
 string Client::render_action_board(){
+    string result;
+    string file;
+    int temp = this->player%2;
+    if(temp==1){
+        file = "player_1.action_board.json";
+    }
+    else{
+        file = "player_2.action_board.json";
+    }
+    for(int i =0; i < file.length(); i++){
+        char x = file.at(i);
+        result += to_string(int(x));
+    }
+    result += "\n";
+    return result;
 }
--- src/Server.cpp
+++ src/Server.cpp
@@ -15,9 +15,9 @@
 //    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
+#include <iomanip>
 #include "common.hpp"
 #include "Server.hpp"
-
 
 /**
  * Calculate the length of a file (helper function)
@@ -25,20 +25,202 @@
  * @param file - the file whose length we want to query
  * @return length of the file in bytes
  */
-int get_file_length(ifstream *file){
+int get_file_length(ifstream * file){
 }
 
 
 void Server::initialize(unsigned int board_size,
                         string p1_setup_board,
                         string p2_setup_board){
+    char line[board_size];
+    this->board_size = board_size;
+
+    if(board_size != BOARD_SIZE){
+        throw(ServerException("Wrong board size"));
+    }
+
+    try{
+        this->p1_setup_board.open(p1_setup_board);
+        if(!this->p1_setup_board.is_open()){
+            throw(ServerException("bad file name"));
+        }
+        this->p1_setup_board.close();
+    }catch(exception e){
+        throw;
+    }
+
+    try{
+        this->p2_setup_board.open(p2_setup_board);
+        int p2Size = 0;
+        if(!this->p2_setup_board.is_open()){
+            throw(ServerException("bad file name"));
+        }
+        while(!this->p2_setup_board.eof()) {
+            this->p2_setup_board >> line;
+            if(!this->p2_setup_board.fail()){
+                ++p2Size;
+            }
+            else{
+                break;
+            }
+        }
+        this->p2_setup_board.close();
+    }
+    catch(exception e) {
+        throw(ServerException("Wrong number of players"));
+    }
 }
 
 
 int Server::evaluate_shot(unsigned int player, unsigned int x, unsigned int y) {
+
+    ifstream board;
+    char line[10];
+
+    if(x >= (board_size)){
+        return OUT_OF_BOUNDS;
+    }
+    else if(y >= (board_size)){
+        return OUT_OF_BOUNDS;
+    }
+    if(player == 1) {
+        p2_setup_board.open("player_2.setup_board.txt");
+        p2_setup_board.clear();
+        p2_setup_board.seekg(0,ios::beg);
+        int size = 0;
+        while(p2_setup_board >> line) {
+            if(size == y){
+                for(int i = 0; i < sizeof(line); i++){
+                    if(i== x){
+                        char temp[6] = SHIPS;
+
+                        for(int j = 0; j < sizeof(temp); j++){
+
+                            if(line[x] == temp[j]){
+                                return HIT;
+                            }
+                            else{
+                                continue;
+                            }
+                        }
+                        return MISS;
+                    }
+                }
+            }
+            size++;
+            line[0] = '\0';
+        }
+        p2_setup_board.close();
+    }
+    else if(player == 2){
+        p1_setup_board.open("player_1.setup_board.txt");
+        p1_setup_board.clear();
+        p1_setup_board.seekg(0,ios::beg);
+        int size = 0;
+        while(p1_setup_board >> line) {
+            if(size == y){
+                for(int i = 0; i < sizeof(line); i++){
+
+                    if(i== x){
+                        char temp[6] = SHIPS;
+
+                        for(int j = 0; j < sizeof(temp); j++){
+
+                            if(line[x] == temp[j]){
+                                return HIT;
+                            }
+                            else{
+                                continue;
+                            }
+                        }
+                        return MISS;
+                    }
+                }
+            }
+            size++;
+            line[0] = '\0';
+
+        }
+        p1_setup_board.close();
+    }
+    else{
+        throw(ServerException("Bad player number"));
+    }
+    return OUT_OF_BOUNDS;
 }
 
 
 int Server::process_shot(unsigned int player) {
-   return NO_SHOT_FILE;
+    ofstream output;
+
+    if (player == 1) {
+        output.open("player_1.result.json");
+    } else if(player == 2){
+        output.open("player_2.result.json");
+    }
+    else{
+        throw(ServerException("Bad number"));
+    }
+
+    string line;
+    int x = 0, y = 0;
+    ifstream input;
+    if (player == 1) {
+        input.open("player_1.shot.json");
+    } else {
+        input.open("player_2.shot.json");
+    }
+
+    noskipws(input);
+    istreambuf_iterator<char> head(input);
+    istreambuf_iterator<char> tail;
+    string data(head, tail);
+
+    for (int i = 0; i < sizeof(data); i++) {
+        if (data[i] == 'x') {
+            x = data[i + 4] - '0';
+            if (data[i + 5] == '0') {
+                x = 10;
+            }
+        } else if ((data[i]) == 'y') {
+            y = data[i + 4] - '0';
+            if (data[i + 5] == '0') {
+                y = 10;
+            }
+            break;
+        }
+    }
+    int response = -2;
+    if (input.is_open()) {
+        response = evaluate_shot(player, x, y);
+    }
+
+    if(!output.good()){
+        throw(ServerException("can't write"));
+    }
+    input.close();
+    remove("player_1.shot.json");
+    remove("player_2.shot.json");
+    output << "{" << endl;
+    if (response == HIT) {
+        output << "    \"result\": 1\n";
+        output << "}";
+        output.close();
+        return SHOT_FILE_PROCESSED;
+    }
+    else if(response == MISS){
+        output << "    \"result\": -1\n";
+        output << "}";
+        output.close();
+        return SHOT_FILE_PROCESSED;
+    }
+    else if(response == OUT_OF_BOUNDS){
+        output << "    \"result\": 0\n";
+        output << "}";
+        output.close();
+        return  SHOT_FILE_PROCESSED;
+    }
+
+    return NO_SHOT_FILE;
+
 }
